1e5c2dd2a75111eeaf936cf78afcd8febfc7dd43
jopamu1314daw2 <jopamu1314daw2@gmail.com>
2014-04-23 13:55:50 +0200

    branca brq00

--- prjava04/src/prjava04/Prjava04.java
+++ prjava04/src/prjava04/Prjava04.java
@@ -14,6 +14,7 @@ public static void main(String[] args) { try {
 InetAddress addr = InetAddress.getLocalHost(); byte[] ipAddr = addr.getAddress();
 String hostname = addr.getHostName(); System.out.println("hostname="+hostname);
 System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+System.out.println("Carpeta personal:" +System.getProperty("user.home"));
 }
 catch (IOException e) { e.printStackTrace();
 }}}
